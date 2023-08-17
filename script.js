10e1278b6ada214cced3cead3080ddbfded03ff8
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-08-17 09:28:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function volume_sphere() {
 	//Write your code here
-	event.preventDefault();
+	preventDefault();
 	let radius = document.getElementById("radius");
 	radius = parseInt(radius.value);
 	let volume = 22*4*radius*radius*radius/(3*7);
-	document.getElementById("volume").value = Math.round(volume);
+	document.getElementById("volume").value = volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere();
